85ea8b031dac4401accaf8d4ceff6bb09071dd42
Duc8101 <vegitobasicinstict1810@gmail.com>
2023-07-01 01:46:16 +0700

    duc

--- CSD201/src/LinkedList/Main.java
+++ CSD201/src/LinkedList/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         MyList my = new MyList();
-        my.AddLast(2);
+        my.AddLast(2);//
         my.AddLast(3);
         my.AddLast(4);
         my.AddFirst(5);
